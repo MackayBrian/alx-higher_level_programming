f67c60b03d2c7add81279f73543bb3e433eb43fe
Mackay Brian <brianmackay091@gmail.com>
2022-11-29 05:58:03 -0800

    task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-    print('{}'.format(number))
-    print(',')
-    number += 1
+    if number == 99:
+        print('{}'.format(number))
+    else:
+        print('{0:2}'.format(number), end=', ')
