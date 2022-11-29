fdcfcb3fd1a61deb9191f4086cf8cf2c1165de83
Mackay Brian <brianmackay091@gmail.com>
2022-11-29 04:33:48 -0800

    task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if number < 0:
 print(f'Last digit of {number} is {last_digit} and is ', end="")
 if last_digit > 5:
     print('greater than 5')
-elif last_digi`t == 0:
+elif last_digit == 0:
     print('0')
 elif last_digit < 6 and last_digit != 0:
     print('less than 6 and not 0')
