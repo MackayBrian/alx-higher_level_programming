29e47c6c73fea3003434269d5d0d8439b7f6ad73
Mackay Brian <brianmackay091@gmail.com>
2022-11-29 05:42:07 -0800

    task 4

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-for number in range(0,99):
-    print('{} = {}'.format(number, hex(number)))
+for number in range(0,99):                              print('{} = {}'.format(number, hex(number)))
