73b28da67c6067e0ef4acb41c5d3faa232bb72cc
Mackay Brian <brianmackay091@gmail.com>
2022-11-30 10:49:20 -0800

    task 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,7 +4,7 @@ def uppercase(str):
         upper_str = ""
         asc = ord(character)
         if asc > 96 and asc < 123:
-            upper_str+=chr(asc - 32)
+            upper_str += chr(asc - 32)
         else:
-            upper_str+=chr(asc)
+            upper_str += chr(asc)
     print('{}'.format(upper_str))
