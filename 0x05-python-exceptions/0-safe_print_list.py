fbb6a15bfdc1f09334ed72ad5fd9b5246aa4e7aa
Mackay Brian <brianmackay091@gmail.com>
2022-12-19 10:45:30 -0800

    task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     for x in my_list:
+            return print(x, end="")
         try:
-            return print(x, end=' ')
-        print()
+            safe_print_lists()
         except Exception:
+            print('Error!')
 
